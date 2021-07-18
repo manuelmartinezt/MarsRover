f378d4bd6724c29074599add7900b536856d8297
Manuel <manuelmartinez20@gmail.com>
2021-07-18 10:38:06 -0400

    Push the completed project

---
+++ MarsRovers/Plateau.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRovers
+{
+    public class Plateau
+    {
+        public static int _x { get; set; }
+        public static int _y { get; set; }
+
+    }
+}
